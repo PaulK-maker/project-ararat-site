bdfeb0838512c676648a97ed97ee544c78e43910
=paul <112735839+PaulK-maker@users.noreply.github.com>
2025-12-15 20:22:04 -0800

    Describe the change

--- script.js
+++ script.js
@@ -20,10 +20,10 @@ document.getElementById('copyZelle')?.addEventListener('click', () => {
 // T-shirt carousel
 const shirts = [
   { src: 'assets/HERO.JPG', caption: 'Design 1 – HERO1' },
-  { src: 'assets/jesus_wins.JPG', caption: 'Design 2 – JESUS' },
+  { src: 'assets/WINS.JPG', caption: 'Design 2 – JESUS' },
   { src: 'assets/PNW.JPG', caption: 'Design 3 – PNW' },
-  { src: 'assets/SHEPHERED.JPG', caption: 'Design 4 – HeRO' },
-  { src: 'assets/ARISE.JPG', caption: 'Design 5 – Joyland Logo' }
+  { src: 'assets/SHEPHERED1.JPG', caption: 'Design 4 – HeRO' },
+  { src: 'assets/ARISE1.JPG', caption: 'Design 5 – Joyland Logo' }
 ];
 
 let currentShirt = 0;
